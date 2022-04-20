ae83177985885671073321ffbbf10d043dee99eb
Andrey Novikov <andrewnovicov@gmail.com>
2022-04-20 23:33:39 +0300

    feat(): add fields to ```CanvasStore``` to store canvas items

--- src/stores/CanvasStore.ts
+++ src/stores/CanvasStore.ts
@@ -3,11 +3,28 @@ type Point = {
     x: number,
     y: number
 }
+type Image = {
+    src: string,
+    x: number,
+    y: number,
+    key?: number
+}
+
 class CanvasStore{
     private absolutePosition : Point = {x: 0, y: 0};
     private scale: number = 1.0;
+    private itemsArray: Image[] = [];
+    private key = 1;
+    chosenImage : string | null = null;
     constructor() {
         makeAutoObservable(this)
+    }
+    addItem(item: Image){
+        item.key = ++this.key
+        this.itemsArray.push(item)
+    }
+    get images(){
+        return this.itemsArray
     }
     set canvasScale(scale: number){
         this.scale = scale
