6ab3580f2025fd57484abdc23f4589365b86cec6
Andrey <andjix_17@mail.ru>
2022-05-10 23:59:53 +0300

    feat(): implement Navigation bar component
    -add drop-down menu for buildings and floors
    -add drop-down for account
    -add navigation buttons
    -add switch buttom
    -change margin-top for Right-slide-menu
    
    Closes: #4, #24, #26

---
+++ src/components/NavBar/NavBarIcon.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
+import SaveIcon from '@mui/icons-material/Save';
+
+interface IconProps {
+    active: any;
+    setActive: any;
+}
+
+const NavBarIcon: React.FC<IconProps> = ({active, setActive}) =>{
+    return (
+        <Stack direction="row" spacing={1}>
+            <IconButton
+                aria-label="icon"
+                onClick={() => setActive(false)}
+                disabled={active}
+                color='inherit'
+            >
+                <DeleteIcon />
+            </IconButton>
+            <IconButton
+                aria-label="icon"
+                onClick={() => setActive(false)}
+                disabled={active}
+                color='inherit'
+            >
+                <SaveIcon/>
+            </IconButton>
+            <IconButton
+                aria-label="icon"
+                onClick={() => setActive(false)}
+                disabled={active}
+                color='inherit'
+            >
+                <UndoIcon/>
+            </IconButton>
+            <IconButton
+                aria-label="icon"
+                onClick={() => setActive(false)}
+                disabled={active}
+                color='inherit'
+            >
+                <RedoIcon/>
+            </IconButton>
+        </Stack>
+    );
+};
+
+export default NavBarIcon;
