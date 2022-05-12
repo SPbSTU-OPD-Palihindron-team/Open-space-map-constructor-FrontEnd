3653ab5b839b58f650c1d7dbb2c5751ae94f3a77
Andrey Novikov <andrewnovicov@gmail.com>
2022-05-12 11:07:33 +0300

    feat(): implement removing context menu on click out of it -the functionality is implemented in `App.tsx` -function `document.querySelector()` sometimes returns null. For that case I add loop, while it's not null. But it's not a good way. Need to code it properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Canvas from "./components/Canvas/Canvas";
 import RightSlideMenu from "./components/RightSlideMenu/RightSlideMenu";
 import NavBar from "./components/NavBar/NavBar";
+import CanvasContextMenu from "./components/Canvas/context_menu/CanvasContextMenu";
 
 
 
 const App: React.FC =() => {
+    let contextMenu = document.querySelector('#canvas__context-menu') as HTMLElement;
+    /*TODO: this is very bad, find how to always get not null but contextMenu*/
+    while(!contextMenu) {
+        contextMenu = document.querySelector('#canvas__context-menu') as HTMLElement;
+    }
+    const hideCanvasContextMenu = () => {
+        if(!contextMenu) return;
+        contextMenu.style.display = 'none';
+    }
     return (
-        <div className="App">
+        <div className="App"
+             onClick={(e) => hideCanvasContextMenu()}
+             onDragStart={(e) => hideCanvasContextMenu()}
+        >
             <NavBar/>
             <Canvas/>
+            <CanvasContextMenu/>
             <RightSlideMenu/>
         </div>
     );
